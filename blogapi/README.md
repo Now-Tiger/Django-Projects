5e8714a6c4e605ee47bfffb4f9e77e17a703c5ff
Swapnil Narwade <swapnil.narwade3@gmail.com>
2023-06-11 05:42:08 +0530

    apis

---
+++ blogapi/README.md
@@ -0,0 +1,16 @@
+# Blog api tutorial
+
+## Introduction
+- Built this tiny project to get started with API developement with Django.
+
+
+## Working
+- Below image shows we have list of blogs on display.
+- When we visite the url _`api/v1/`_ route return/gets all the blogs from the database. (__GET__ method)
+  
+![image](./imgs/blog-posts-apis.jpg)
+
+- In below image I've posted raw data via postman (basically performed __POST__ method)
+- result can be seen on above image, as the data refaltes on our _`/api/v1/`_ url and database.
+
+![image](./imgs/post-method-successfully.jpg)
---
+++ blogapi/posts/tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from .models import Post
+
+# Create your tests here.
+
+
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        testuser1 = User.objects.create_user(
+            username="swapnil", password="daisy2021")
+        testuser1.save()
+
+        # - create blog post
+        test_post = Post.objects.create(
+            author=testuser1, title="Title created in testing", body="Testing content...")
+        test_post.save()
+
+    def test_blog_content(self) -> None:
+        post = Post.objects.get(id=1)
+        athr = f'{post.author}'
+        ttle = f'{post.title}'
+        body = f'{post.body}'
+        self.assertEqual(athr, 'swapnil')
+        self.assertEqual(ttle, 'Title created in testing')
+        self.assertEqual(body, 'Testing content...')
