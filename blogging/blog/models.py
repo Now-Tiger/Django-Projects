624970ee06a3d2c2105edf791d763fc3cc73cf0a
Swapnil Narwade <swapnil.narwade3@gmail.com>
2023-07-03 03:00:38 +0530

    applied new migrations on model.py

--- blogging/blog/models.py
+++ blogging/blog/models.py
@@ -7,35 +7,27 @@ from django.utils import timezone
 
 class PublishedManager(models.Manager):
     def get_queryset(self) -> QuerySet:
-        return super(PublishedManager, self)\
-            .get_queryset()\
-            .filter(status='published')
+        return super(PublishedManager, self).get_queryset().filter(status="published")
 
 
 class Post(models.Model):
-    STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published')
-    )
+    STATUS_CHOICES = (("draft", "Draft"), ("published", "Published"))
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250,
-                            unique_for_date='publish')
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,
-                              choices=STATUS_CHOICES,
-                              default='draft')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
 
     objects = models.Manager()
     published = PublishedManager()
 
     class Meta:
-        ordering = ('-publish', )
+        ordering = ("-publish",)
 
     def __str__(self) -> str:
         return self.title
@@ -43,10 +35,5 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse(
             "blog:post_detail",
-            args=[
-                self.publish.year,
-                self.publish.month,
-                self.publish.day,
-                self.slug
-            ]
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
         )
