d078e6eb490f37f89443389eb68dfce04145dab4
Swapnil Narwade <swapnil.narwade3@gmail.com>
2023-10-02 19:42:03 +0530

    removed crud_postgres folder

--- crud_postgres/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# USER -> id(AutoField), name(CharField), gender(CharField), age(IntegerField)
-class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    gender = models.CharField(max_length=10)
-    age = models.IntegerField()
--- crud_postgres/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserListCreate, UserRetriveUpdateDelete
-
-urlpatterns = [
-    path('users', UserListCreate.as_view(), name='Create-User-List'),
-    path('users/<int:pk>', UserRetriveUpdateDelete.as_view(), name='user-Details')
-]
